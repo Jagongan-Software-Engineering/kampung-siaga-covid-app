7a35e0a7c74f8ae2a77b242cbd93f59c342509ae
derysudrajat <dery.sudrajat17@gmail.com>
2020-04-07 22:26:16 +0700

    1. update ui for provinsi item 2. add method for searching data and bug fixing

--- app/src/main/java/com/seadev/kampungsiagacovid/adapter/ProvinsiAdapter.java
+++ app/src/main/java/com/seadev/kampungsiagacovid/adapter/ProvinsiAdapter.java
@@ -34,6 +34,18 @@ public class ProvinsiAdapter extends RecyclerView.Adapter<ProvinsiAdapter.Provin
         this.provinsiList = provinsiList;
     }
 
+    public void addDaraProvinsi(DataProvinsi dataProvinsi) {
+        this.provinsiList.add(dataProvinsi);
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        int size = getProvinsiList().size();
+        provinsiList.clear();
+        notifyItemRangeRemoved(0, size - 1);
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public ProvinsiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -46,10 +58,12 @@ public class ProvinsiAdapter extends RecyclerView.Adapter<ProvinsiAdapter.Provin
     @Override
     public void onBindViewHolder(@NonNull ProvinsiViewHolder holder, int position) {
         DataProvinsi dataProvinsi = provinsiList.get(position);
-        holder.textView.setText(dataProvinsi.getAttribute().getNamaProv() + "\n" +
-                "Positiv  : " + dataProvinsi.getAttribute().getKasusPositifl() + "\n" +
-                "Sembuh   : " + dataProvinsi.getAttribute().getKasusSembuh() + "\n" +
-                "Meninggal: " + dataProvinsi.getAttribute().getKasusKasusManinggal());
+        if (!dataProvinsi.getAttribute().getNamaProv().toLowerCase().equals("indonesia")) {
+            holder.provinsiName.setText(dataProvinsi.getAttribute().getNamaProv());
+            holder.positifCase.setText(String.valueOf(dataProvinsi.getAttribute().getKasusPositifl()));
+            holder.recoverCase.setText(String.valueOf(dataProvinsi.getAttribute().getKasusSembuh()));
+            holder.deathCase.setText(String.valueOf(dataProvinsi.getAttribute().getKasusKasusManinggal()));
+        }
     }
 
     @Override
@@ -58,8 +72,14 @@ public class ProvinsiAdapter extends RecyclerView.Adapter<ProvinsiAdapter.Provin
     }
 
     public class ProvinsiViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.tv_provinsi)
-        TextView textView;
+        @BindView(R.id.tv_item_provinsi)
+        TextView provinsiName;
+        @BindView(R.id.tv_item_positif_prov)
+        TextView positifCase;
+        @BindView(R.id.tv_item_sembuh_prov)
+        TextView recoverCase;
+        @BindView(R.id.tv_item_meni_prov)
+        TextView deathCase;
 
         public ProvinsiViewHolder(@NonNull View itemView) {
             super(itemView);
