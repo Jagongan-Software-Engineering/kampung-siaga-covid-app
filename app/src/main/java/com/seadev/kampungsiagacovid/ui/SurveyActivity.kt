caa2c3f926aae16b8894381961ace4acce6f02e5
derysudrajat <dery.sudrajat17@gmail.com>
2020-04-07 22:26:38 +0700

    add back nav

--- app/src/main/java/com/seadev/kampungsiagacovid/ui/SurveyActivity.kt
+++ app/src/main/java/com/seadev/kampungsiagacovid/ui/SurveyActivity.kt
@@ -3,6 +3,7 @@ package com.seadev.kampungsiagacovid.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.view.MenuItem
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -26,10 +27,17 @@ class SurveyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey)
+        supportActionBar?.title = "Koesioner Penilaian Diri"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         survey = findViewById(R.id.survey_view)
         container = findViewById(R.id.surveyContainer)
         initialQuestion()
         setupSurvey(survey)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupSurvey(surveyView: SurveyView) {
