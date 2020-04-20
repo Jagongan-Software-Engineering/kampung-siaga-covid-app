d2a9c5da09da1328a3e1d7a733ed7f8f02b345f2
derysudrajat <dery.sudrajat17@gmail.com>
2020-04-20 08:39:54 +0700

    add Users model

---
+++ app/src/main/java/com/seadev/aksi/model/Users.kt
@@ -0,0 +1,14 @@
+package com.seadev.aksi.model
+
+data class Users(
+        var idUser: String? = "",
+        var phone: String? = "",
+        var nama: String? = "",
+        var alamat: String? = "",
+        var idProvinsi: String? = "",
+        var idKotaKab: String? = "",
+        var idKacamatan: String? = "",
+        var idDesa: String? = "",
+        var rtrw: String? = "",
+        var nik: String? = ""
+)
