311fbbe1b79ca99fb48654b98323a4af488a59a5
derysudrajat <dery.sudrajat17@gmail.com>
2020-08-29 13:33:52 +0700

    modify: change unit test to kotlin version

--- app/src/test/java/com/seadev/aksi/ExampleUnitTest.kt
+++ app/src/test/java/com/seadev/aksi/ExampleUnitTest.kt
@@ -1,106 +1,106 @@
-package com.seadev.aksi;
+package com.seadev.aksi
 
-import android.util.Log;
-
-import com.seadev.aksi.model.dataapi.DataHarian;
-import com.seadev.aksi.model.requestbody.ItemDataHarian;
-import com.seadev.aksi.rest.ApiClientNasional;
-import com.seadev.aksi.rest.ApiInterfaceNasional;
-
-import org.junit.Test;
-
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import static org.junit.Assert.assertEquals;
+import android.util.Log
+import com.seadev.aksi.model.dataapi.DataHarian
+import com.seadev.aksi.model.requestbody.ItemDataHarian
+import com.seadev.aksi.rest.ApiClientNasional
+import com.seadev.aksi.rest.ApiInterfaceNasional
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-
-public class ExampleUnitTest {
-    private List<DataHarian> dataHarianList;
+class ExampleUnitTest {
+    private var dataHarianList: List<DataHarian>? = null
 
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2.toLong())
     }
 
     @Test
-    public void testDate() {
-        String date = LocalDateTime.ofInstant(
-                Instant.ofEpochMilli(Long.valueOf("1592265600000")),
+    fun testDate() {
+        val date = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(java.lang.Long.valueOf("1592265600000")),
                 ZoneId.systemDefault()
-        ).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-        System.out.println(date);
-
-        Date date1 = new Date(Long.valueOf("1592265600000"));
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String mDate = formatter.format(date1);
-        String now = formatter.format(new Date());
-
-        System.out.println(now);
-        System.out.println(mDate);
-
-        if (now.equals(date)) {
-            System.out.println("Same day");
+        ).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        println(date)
+        val date1 = Date(java.lang.Long.valueOf("1592265600000"))
+        val formatter = SimpleDateFormat("yyyy-MM-dd")
+        val mDate = formatter.format(date1)
+        val now = formatter.format(Date())
+        println(now)
+        println(mDate)
+        if (now == date) {
+            println("Same day")
         }
     }
 
     @Test
-    public void testData() {
-        dataHarianList = new ArrayList<>();
-        ApiInterfaceNasional apiServiceNasional = ApiClientNasional.getClientNasional().create(ApiInterfaceNasional.class);
-        Call<ItemDataHarian> call = apiServiceNasional.getDataHarian();
-        call.enqueue(new Callback<ItemDataHarian>() {
-            @Override
-            public void onResponse(Call<ItemDataHarian> call, Response<ItemDataHarian> response) {
-                assert response.body() != null;
-                dataHarianList = response.body().getDataHarianList();
-                int index = 0;
-                for (DataHarian dataHarian : dataHarianList) {
-
-                    Date date1 = new Date(dataHarian.getAttributes().getTgl());
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-                    String mDate = formatter.format(date1);
-                    String now = formatter.format(new Date());
-                    System.out.println("mDate: " + mDate);
-                    System.out.println("Now: " + now);
-
-                    if (now.equals(mDate)) {
-                        System.out.println("Same day");
-                        if (dataHarian.getAttributes().getKasusTotal() != 0) {
-                            index++;
-                            break;
+    fun testData() {
+        val apiServiceNasional = ApiClientNasional.getClientNasional().create(ApiInterfaceNasional::class.java)
+        val call = apiServiceNasional.dataHarian
+        call.enqueue(object : Callback<ItemDataHarian?> {
+            override fun onResponse(call: Call<ItemDataHarian?>, response: Response<ItemDataHarian?>) {
+                assert(response.body() != null)
+                val dataHarianList = response.body()!!.dataHarianList
+                var index = 0
+                for (dataHarian in dataHarianList) {
+                    val date1 = Date(dataHarian.attributes.tgl)
+                    val formatter = SimpleDateFormat("yyyy-MM-dd")
+                    val mDate = formatter.format(date1)
+                    val now = formatter.format(Date())
+                    println("mDate: $mDate")
+                    println("Now: $now")
+                    if (now == mDate) {
+                        println("Same day")
+                        if (dataHarian.attributes.kasusTotal != 0) {
+                            index++
+                            break
                         } else {
-                            break;
+                            break
                         }
                     } else {
-                        index++;
+                        index++
                     }
                 }
-                DataHarian dataHarian = dataHarianList.get(index - 1);
-                System.out.println("Total: " + dataHarian.getAttributes().getKasusTotal());
+                val dataHarian = dataHarianList.get(index - 1)
+                println("Total: " + dataHarian.attributes.kasusTotal)
             }
 
-            @Override
-            public void onFailure(Call<ItemDataHarian> call, Throwable t) {
-                Log.d("MainActivity", "error: " + t);
+            override fun onFailure(call: Call<ItemDataHarian?>, t: Throwable) {
+                Log.d("MainActivity", "error: $t")
             }
-        });
+        })
+    }
+
+    @Test
+    fun testPrecentace() {
+        val inCare = (22797.toDouble() / 1000)
+        val cured = (72881.toDouble() / 1000)
+        val die = (4322.toDouble() / 1000)
+        val newincare = BigDecimal(inCare).setScale(2, RoundingMode.HALF_EVEN)
+        val newcured = BigDecimal(cured).setScale(2, RoundingMode.HALF_EVEN)
+        val newdie = BigDecimal(die).setScale(2, RoundingMode.HALF_EVEN)
+
+        Assert.assertEquals("In Care", 22.80.toFloat(), newincare.toFloat())
+        Assert.assertEquals("Recovered", 72.88.toFloat(), newcured.toFloat())
+        Assert.assertEquals("Die", 4.32.toFloat(), newdie.toFloat())
+        val total = newincare + newcured + newdie
+        Assert.assertEquals("Total Percentage", 100, total.toInt())
     }
 }
